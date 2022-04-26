0750c20dff1197abd77dcf8ef36091076b960955
becem brahmi <becembrahmi@gmail.com>
2022-04-26 10:25:46 +0100

    animation first steps

--- scene.py
+++ scene.py
@@ -1,16 +1,23 @@
 from manim import *
 
 
-class SquareAndCircle(Scene):
+class CircleToTop(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))
-        self.play(circle.animate.set_fill(WHITE,opacity=1))
-        text = Text("Hello world", font_size=16,color=BLACK)
-        self.play(Write(text))
-        self.play(circle.animate.shift(2 * RIGHT),
-                  text.animate.shift(2 * RIGHT))
-
-        self.wait()
-
+        circle = Circle(radius=1)  # create a circle
+        circle.set_fill(WHITE, opacity=0.9)  # set the color and transparency
+        self.play(GrowFromCenter(circle))
+        # self.play(circle.animate.set_fill(WHITE,opacity=0.8))
+        text = Text("165", font_size=22, color=BLACK)
+        self.play(Write(text, color=BLACK))
+        self.play(circle.animate.shift(2 * UP).scale(0.7),
+                  text.animate.shift(2 * UP).scale(0.7))
+        toRight = Line([0, 1.3, 0], [1, 0, 0])
+        toLeft = Line([0, 1.3, 0], [-1, 0, 0])
+        leftNode = Circle(radius=0.5).shift(1*RIGHT + 0.5*DOWN).set_fill(color=WHITE,opacity=0.9)
+        rightNode = Circle(radius=0.5).shift(0.5*DOWN + 1*LEFT).set_fill(color=WHITE,opacity=0.9)
+        text = Text("170", font_size=22, color=BLACK).shift(1*RIGHT + 0.5*DOWN)
+        text1 = Text("150", font_size=22, color=BLACK).shift(0.5*DOWN + 1*LEFT)
+        self.play(Create(toRight), Create(toLeft))
+        self.play(GrowFromCenter(leftNode), GrowFromCenter(rightNode))
+        self.play(Write(text),Write(text1))
+        self.wait(3)
